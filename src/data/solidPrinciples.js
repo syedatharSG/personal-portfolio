cf9e319e47600d6312d2d73661ffe2135b96ac18
syedathar2 <syedathar319@gmail.com>
2023-11-02 13:13:39 -0500

    Update solidPrinciples.js

--- src/data/solidPrinciples.js
+++ src/data/solidPrinciples.js
@@ -2,7 +2,8 @@ export const solidPrinciple = {
     title: "SOLID Princples",
     date: "October 26, 2023",
     content:     <>
-
+    <p className="mb-8"> The SOLID principles are a set of guidelines for writing efficient and scalable code. These guidelines were introduced by Robert C. Martin to 
+    help developers write code that is not only easy to understand and modify but also avoids common pitfalls that developers encounter while writing code. </p>
     </>,
     imageUrl: ""
 }
