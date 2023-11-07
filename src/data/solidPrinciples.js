a9f519e16bf10cfe82b1d0ffd88b3d735f6ffd66
syedathar2 <syedathar319@gmail.com>
2023-11-07 17:13:56 -0600

    Continue writing article 5

--- src/data/solidPrinciples.js
+++ src/data/solidPrinciples.js
@@ -1,4 +1,5 @@
 import solidArticleImage from '../images/solid.png';
+import SRPExample1 from '../images/SRPExample1.png';
 
 
 export const solidPrinciple = {
@@ -19,7 +20,16 @@ export const solidPrinciple = {
     one responsibility, and it should carry out that responsibility well.  </p>
 
     <p className="mb-8">When a class has several responsibilities, it becomes harder to manage and understand the class. Changing one responsibility may 
-    affect other responsibility as well, which can lead to unintended consequences and more bugs in a system. </p>
+    affect other responsibilities as well leading to unintended consequences and more bugs in a system. Here's an example that violates SRP (Single Responsibility Principle):</p>
+
+    <img src={SRPExample1} alt="SRP Violation Example" className="mt-8 mb-8 flex justify-center items-center w-40 h-48 object-cover rounded-md mx-auto" />
+    
+    <p className='mb-8'>The <strong>Invoice</strong> class violates SRP because there are several reasons it may change. For instance, if the logic for <strong>calculateTotal()</strong> changes, 
+    the class would be modified. Similarly, if we need to change the way an invoice prints or is saved 
+    to the database, the <strong>printInvoice()</strong> and <strong>saveToDb()</strong> methods would be altered. There are several reasons for changing the class, and we know
+    a class should have only one responsibility according to SRP. Because the <strong>Invoice</strong> class doesn't adhere to this standard, it violates SRP.</p>
+
+
     </>,
     imageUrl: solidArticleImage
 }
